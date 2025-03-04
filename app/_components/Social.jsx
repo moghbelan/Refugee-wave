b08eb60e92905411b3eb06918af7aba6a1513457
moghbelan <nmoghbelan@yahoo.com>
2025-03-04 13:49:44 +0100

    socialChange

--- app/_components/Social.jsx
+++ app/_components/Social.jsx
@@ -7,27 +7,35 @@ import Facebook from "../../public/Facebook.svg";
 export default function Social() {
   return (
     <div className="flex space-x-4 mt-3">
-      <a
-        href="https://TikTok.com"
-        className="text-gray-700 hover:text-blue-500"
-      >
-        <Image src={TikTok} width={50} height={50} alt="Twitter" />
-      </a>
-      <a
-        href="https://Instagram.com"
-        className="text-gray-700 hover:text-blue-500"
-      >
-        <Image src={Instagram} width={50} height={50} alt="Twitter" />
-      </a>
-      <a href="https://x.com" className="text-gray-700 hover:text-blue-500">
-        <Image src={Twitter} width={50} height={50} alt="Twitter" />
-      </a>
-      <a
-        href="https://Facebook.com"
-        className="text-gray-700 hover:text-blue-500"
-      >
-        <Image src={Facebook} width={50} height={50} alt="Twitter" />
-      </a>
+      {session && (
+        <a
+          href="https://TikTok.com"
+          className="text-gray-700 hover:text-blue-500"
+        >
+          <Image src={TikTok} width={50} height={50} alt="Twitter" />
+        </a>
+      )}
+      {session && (
+        <a
+          href="https://Instagram.com"
+          className="text-gray-700 hover:text-blue-500"
+        >
+          <Image src={Instagram} width={50} height={50} alt="Twitter" />
+        </a>
+      )}
+      {session && (
+        <a href="https://x.com" className="text-gray-700 hover:text-blue-500">
+          <Image src={Twitter} width={50} height={50} alt="Twitter" />
+        </a>
+      )}
+      {session && (
+        <a
+          href="https://Facebook.com"
+          className="text-gray-700 hover:text-blue-500"
+        >
+          <Image src={Facebook} width={50} height={50} alt="Twitter" />
+        </a>
+      )}
     </div>
   );
 }
